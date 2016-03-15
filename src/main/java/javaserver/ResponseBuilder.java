ca72abb9e2104cf9faed6e7f0905a2ca0a23c595
Cat McLoughlin <catmclough@gmail.com>
2016-03-15 13:50:29 -0700

    Test decoded params are returned in response body

--- src/main/java/javaserver/ResponseBuilder.java
+++ src/main/java/javaserver/ResponseBuilder.java
@@ -11,12 +11,12 @@ public class ResponseBuilder {
 	protected HashMap<String, String> responseParts;
 	protected HashMap<String, String> responseCodes;
 	protected HashMap<String, String[]> okRequests;
-	
+
 	ResponseBuilder() {
 		this.responseParts = new HashMap<String, String>();
 		this.okRequests = new HashMap<String, String[]>();
 		setOKRequests();
-		
+
 		this.responseCodes = new HashMap<String, String>();
 		setResponseCodes();
 	}
@@ -26,7 +26,7 @@ public class ResponseBuilder {
 		okRequests.put("/form", OK_FORM_REQUESTS);
 		okRequests.put("/method_options", OK_METHOD_OPTIONS);
 	}
-	
+
 	private void setResponseCodes() {
 		responseCodes.put("200", "HTTP/1.1 200 OK");
 		responseCodes.put("404", "HTTP/1.1 404 Not Found");
@@ -38,12 +38,12 @@ public class ResponseBuilder {
 		response += responseParts.get("Header");
 		return response;
 	}
-	
+
 	private void setResponseParts(HashMap<String, String> request) {
 		this.responseParts.put("Response Code", getResponseCode(request));
 		this.responseParts.put("Header", getResponseHeader(request));
 	}
-	
+
 	private String getResponseCode(HashMap<String, String> request) {
 		String[] methodOptions = okRequests.get(request.get("URI"));
 		String requestType = request.get("Type");
@@ -53,7 +53,7 @@ public class ResponseBuilder {
 			return responseCodes.get("404");
 		}
 	}
-	
+
 	private String getResponseHeader(HashMap<String, String> request) {
 		String header = "";
 		if (request.get("URI").equals("/method_options")) {
--- src/test/java/javaserver/ResponseBuilderTest.java
+++ src/test/java/javaserver/ResponseBuilderTest.java
@@ -89,4 +89,18 @@ public class ResponseBuilderTest {
 		String methodOptionsHeader = "\r\nAllow: GET,HEAD,POST,OPTIONS,PUT\r\n";
 		assertEquals(testResponseBuilder.responseParts.get("Header"), methodOptionsHeader);
 	}
+	
+	@Test
+	public void testDecodedParamsInResponseBody() {
+		String codedURI = "/parameters?variable_1=Operators%20%3C%2C%20%3E%2C%20%3D%2C%20!%3D%3B%20%2B%2C%20-%2C%20*%2C%20%26%2C%20%40%2C%20%23%2C%20%24%2C%20%5B%2C%20%5D%3A%20%22is%20that%20all%22%3F&variable_2=stuff";
+		String decodedParamOne = "variable_1 = Operators <, >, =, !=; +, -, *, &, @, #, $, [, ]: \"is that all\"?";
+		String decodedParamTwo = "variable_2 = stuff";
+		request.put("Type", "GET");
+		request.put("URI", codedURI);
+		
+		testResponseBuilder.getResponse(request);
+		String responseBody = testResponseBuilder.responseParts.get("Body");
+		assertTrue(responseBody.contains(decodedParamOne));
+		assertTrue(responseBody.contains(decodedParamTwo));
+	}
 }
