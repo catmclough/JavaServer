6e81a38366d6670e00a52516d1714cb82ebf5cc0
Cat McLoughlin <catmclough@gmail.com>
2016-03-15 14:22:34 -0700

    Add test to check response code for coded parameter request

--- src/test/java/javaserver/ResponseBuilderTest.java
+++ src/test/java/javaserver/ResponseBuilderTest.java
@@ -20,6 +20,7 @@ public class ResponseBuilderTest {
 	
 	HashMap<String, String> request;
 
+	String codedURI = "/parameters?variable_1=Operators%20%3C%2C%20%3E%2C%20%3D%2C%20!%3D%3B%20%2B%2C%20-%2C%20*%2C%20%26%2C%20%40%2C%20%23%2C%20%24%2C%20%5B%2C%20%5D%3A%20%22is%20that%20all%22%3F&variable_2=stuff";
 	@Before
 	public void setUp() {
 		request = new HashMap<String, String>();
@@ -92,7 +93,6 @@ public class ResponseBuilderTest {
 	
 	@Test
 	public void testDecodedParamsInResponseBody() {
-		String codedURI = "/parameters?variable_1=Operators%20%3C%2C%20%3E%2C%20%3D%2C%20!%3D%3B%20%2B%2C%20-%2C%20*%2C%20%26%2C%20%40%2C%20%23%2C%20%24%2C%20%5B%2C%20%5D%3A%20%22is%20that%20all%22%3F&variable_2=stuff";
 		String decodedParamOne = "variable_1 = Operators <, >, =, !=; +, -, *, &, @, #, $, [, ]: \"is that all\"?";
 		String decodedParamTwo = "variable_2 = stuff";
 		request.put("Type", "GET");
@@ -102,5 +102,14 @@ public class ResponseBuilderTest {
 		String responseBody = testResponseBuilder.responseParts.get("Body");
 		assertTrue(responseBody.contains(decodedParamOne));
 		assertTrue(responseBody.contains(decodedParamTwo));
+	}
+	
+	@Test
+	public void testCodedParamsGetsNoResponseCode() {
+		request.put("Type", "GET"); 
+		request.put("URI", codedURI);
+		
+		testResponseBuilder.getResponse(request);
+		assertTrue(testResponseBuilder.responseParts.get("Response Code").isEmpty());
 	}
 }
