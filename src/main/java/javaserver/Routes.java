ad973d908ecf5c7651d3b2657c774394759dffea
Cat McLoughlin <catmclough@gmail.com>
2016-03-17 12:05:26 -0700

    Change names of supported routes from 'OK...' to 'SUPPORTED...'

--- src/main/java/javaserver/Routes.java
+++ src/main/java/javaserver/Routes.java
@@ -3,17 +3,17 @@ package javaserver;
 import java.util.HashMap;
 
 public class Routes {
-	private static final String[] OK_ROOT_REQUESTS = {"GET"};
-	private static final String[] OK_FORM_REQUESTS = {"GET", "POST", "PUT"};
-	private static final String[] OK_METHOD_OPTIONS = {"GET", "HEAD", "POST", "OPTIONS", "PUT"};
+	private static final String[] SUPPORTED_ROOT_REQUESTS = {"GET"};
+	private static final String[] SUPPORTED_FORM_REQUESTS = {"GET", "POST", "PUT"};
+	private static final String[] SUPPORTED_METHOD_OPTIONS = {"GET", "HEAD", "POST", "OPTIONS", "PUT"};
 
 	public static HashMap<String, String[]> routeOptions;
 
 	public static void configure() {
 		routeOptions = new HashMap<String, String[]>();
-		routeOptions.put("/", OK_ROOT_REQUESTS);
-		routeOptions.put("/form", OK_FORM_REQUESTS);
-		routeOptions.put("/method_options", OK_METHOD_OPTIONS);
+		routeOptions.put("/", SUPPORTED_ROOT_REQUESTS);
+		routeOptions.put("/form", SUPPORTED_FORM_REQUESTS);
+		routeOptions.put("/method_options", SUPPORTED_METHOD_OPTIONS);
 	}
 
 	public static String[] getOptions(String route) {
