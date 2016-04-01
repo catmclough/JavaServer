45862381c8daae409ebbe71ab62c3b1ca561fb9e
Cat McLoughlin <cmcloughlin@8thlight.com>
2016-03-31 17:47:49 -0700

    Handle user arguments that specify port and/or public directory

--- src/main/java/javaserver/App.java
+++ src/main/java/javaserver/App.java
@@ -1,31 +1,47 @@
 package javaserver;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 
 public class App {
 
-	protected static final int PORT = 5000;
+	public static String name = "Cat's Java Server";
+	protected static final int DEFAULT_PORT = 5000;
+	protected static int port;
+	protected static final String DEFAULT_PUBLIC_DIRECTORY = "public";
+	protected static PublicDirectory publicDirectory = new PublicDirectory(DEFAULT_PUBLIC_DIRECTORY);
 	protected static Server server;
-	protected static boolean isOn = false;
 
 	public static void main(String[] args) throws IOException {
-		setUpServer();
+		setUpServer(args);
 		runServer(server);
 	}
 
-	protected static void setUpServer() throws IOException {
-		server = new Server(new ServerSocket(PORT), new ThreadManager());
+	protected static void setUpServer(String[] args) throws IOException {
+		port = ArgHandler.getPort(args, DEFAULT_PORT);
+		setDirectory(args);
+		server = new Server(new ServerSocket(port));
 	}
 
-	protected static void runServer(Server server) throws IOException {
-		isOn = true;
-		while (isOn) {
-			server.run();
+	private static void setDirectory(String[] args) {
+		String specifiedDirectoryName = ArgHandler.getChosenDirectoryName(args);
+		if (specifiedDirectoryName != null) {
+			publicDirectory = new PublicDirectory(specifiedDirectoryName);
+			if (!directoryExists(publicDirectory.getDirectoryName())) {
+				System.out.println(publicDirectory.getDirectoryName() + " does not exist.");
+				throw new Error();
+			}
 		}
 	}
 
-	protected static void close() {
-		isOn = false;
+	protected static void runServer(Server server) throws IOException {
+		System.out.println("Server running on port " + port + "\nPublic Directory: " + publicDirectory.getRoute());
+		server.run();
+	}
+
+	private static boolean directoryExists(String directoryName) {
+		File directory = new File(directoryName);
+		return directory.exists();
 	}
 }
---
+++ src/main/java/javaserver/PublicDirectory.java
@@ -0,0 +1,33 @@
+package javaserver;
+
+import java.io.File;
+
+public class PublicDirectory {
+
+	private String route;
+
+	public PublicDirectory(String route) {
+		this.route = route;
+	}
+
+	public String[] getDirectoryListing() {
+		File directory = new File(getDirectoryName());
+		return directory.list();
+	}
+
+	public String getRoute() {
+		if (route.startsWith("/")) {
+			return route;
+		} else {
+			return "/" + route;
+		}
+	}
+
+	public String getDirectoryName() {
+		if (route.startsWith("/")) {
+			return route.substring(1);
+		} else {
+			return route;
+		}
+	}
+}
